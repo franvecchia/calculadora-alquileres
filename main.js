01991fd0b153638428bcb716f4107d10fd7b9903
Francisco Vecchia <franvecchia00@gmail.com>
2024-04-16 13:49:30 -0300

    funcionalidad

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-let boton = document.getElementById('boton')
+const boton = document.getElementById('boton')
 
 boton.addEventListener('click', (e) => {
     e.preventDefault()
 
-    let fechaInicio = document.getElementById("fechaInicio").value
-    let fechaFinalizacion = document.getElementById("fechaFinal").value
-    let monto = document.getElementById('monto').value
-    calcularFechaInicial(fechaInicio)
-})
+    const fechaInicio = document.getElementById("fechaInicio").value
+    const fechaFinalizacion = document.getElementById("fechaFinal").value
+    const monto = document.getElementById('monto').value
 
+    if (fechaInicio != "" && fechaFinalizacion != "" && monto != "" && fechaInicio<fechaFinalizacion) {
+        calcularDatos(fechaInicio, fechaFinalizacion, monto)
+    } else {
+        Swal.fire({
+            title: "Ingrese los datos correctamente.",
+            icon: "error",
+            confirmButtonColor: '#003256'
+          })
+    }
+})
 
 async function obtenerDatos () {
     const apiUrl = "inflacion_mensual_oficial"
@@ -17,21 +25,53 @@ async function obtenerDatos () {
     const peticion = await fetch(`${proxyUrl}/${apiUrl}`, {
         headers: {
             Authorization: "BEARER eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDQ0MzMwNTgsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJmcmFudmVjY2hpYTAwQGdtYWlsLmNvbSJ9.LxijwX08kVJGZ5iJv6T8kS3kOO7q6z7r-16gB8TFTaIa8K7zVEcpUmrKRphC4JGq6YlhAZLzy3dldOfr5zT4vQ",
+            // 11/4/25
         },
     })
     .then((response) => {return response.json()})
-    .then((data) => {return data})
+    .then((data) => {
+        console.log(data)
+        return data})
     .catch((err) => {return err})
 
     return peticion
 }
 
-async function calcularFechaInicial(fechaInicial) {
+async function calcularDatos(fechaInicial, fechaFinalizacion, monto) {
     let arr = await obtenerDatos()
-    let fechaBuscada = arr.find((fechaAux) => fechaAAnioMes(fechaAux.d) === fechaAAnioMes(fechaInicial))
+    let i=0
+    let fechaBuscada=null
+    let inflacion=0
+    let montoAcum=monto
+    
+    while (i<arr.length && fechaBuscada==null) {
+        if (fechaAAnioMes(arr[i].d) === fechaAAnioMes(fechaInicial)) {
+            fechaBuscada=arr[i]
+            let FechaFinal= arr.find((fechaAux) => fechaAAnioMes(fechaAux.d) === fechaAAnioMes(fechaFinalizacion))
+            if (FechaFinal == null) {
+                i--
+                fechaBuscada=arr[i]
+                let size=arr.length
+                FechaFinal=arr[size-1]
+            }
 
-    console.log(fechaBuscada)
-    return fechaBuscada
+            while (fechaAAnioMes(fechaBuscada.d) != fechaAAnioMes(FechaFinal.d)) {
+                inflacion=parseFloat(fechaBuscada.v)
+                montoAcum*=(1+(inflacion/100))
+                i++
+                fechaBuscada=arr[i]
+            }
+            
+            inflacion=parseFloat(fechaBuscada.v)
+            montoAcum*=(1+(inflacion/100))
+            i++
+            fechaBuscada=arr[i]
+        } else {
+            i++
+        }
+    }
+
+    console.log(montoAcum)
 }
 
 function fechaAAnioMes(fecha) {
